584abfaf523978549d2638c845cb6e6a3d4cc998
Ana Raro <ana.raro01@gmail.com>
2020-09-06 19:18:56 +0100

    Added/Config of NelmioApiDocBundle

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,5 @@ return [
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
 ];
